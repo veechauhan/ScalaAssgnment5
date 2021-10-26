ff4168567ea8166ca8c91295cf60cd6504d0af7e
Vineet Chauhan <86958280+veechauhan@users.noreply.github.com>
2021-10-26 18:20:36 +0530

    Update Union.scala

--- src/main/scala/edu/KUP/Union.scala
+++ src/main/scala/edu/KUP/Union.scala
@@ -2,8 +2,7 @@ package edu.KUP
 
 class Union {
   def unions(set1: Set[Int], set2: Set[Int]): Set[Int] = {
-    val tempSet: Set[Int] = set1 ++ set2
-    tempSet
-  }
+   set1 ++ set2
+      }
 }
 
